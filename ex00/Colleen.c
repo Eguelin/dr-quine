761252d2fecbe8f12b0fbc8f67e4bd3a7f516587
Eguelin <eguelin@student.42lyon.fr>
2025-01-24 13:53:02 +0100

    fix: Update ft_creat to return an error code and improve error handling

--- ex00/Colleen.c
+++ ex00/Colleen.c
@@ -3,19 +3,21 @@
 #include <stdio.h>
 
 /*I'm so stupid*/
-void	ft_creat(char *x)
+int	ft_creat(char *x)
 {
-	int fd = open("tmp_Colleen", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	int	fd = open("tmp_Colleen", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
-		return;
-	dprintf(fd, x, 10, 10, 10, 10, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10, 10, 9, 10, 10, 9, 10, 9, 34, x, 34, 10, 9, 10, 9, 10, 10);
+		return(1);
+	dprintf(fd, x, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, x, 34, 10, 10, 10, 10, 10);
 	close(fd);
+	return(0);
 }
 
 int	main(void)
 {
 	/*but really!*/
-	char *x = "#include <unistd.h>%c#include <fcntl.h>%c#include <stdio.h>%c%c/*I'm so stupid*/%cvoid%cft_creat(char *x)%c{%c%cint fd = open(%ctmp_Colleen%c, O_WRONLY | O_CREAT | O_TRUNC, 0644);%c%cif (fd == -1)%c%c%creturn;%c%cdprintf(fd, x, 10, 10, 10, 10, 10, 9, 10, 10, 9, 34, 34, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10, 10, 9, 10, 10, 9, 10, 9, 34, x, 34, 10, 9, 10, 9, 10, 10);%c%cclose(fd);%c}%c%cint%cmain(void)%c{%c%c/*but really!*/%c%cchar *x = %c%s%c;%c%cft_creat(x);%c%creturn (0);%c}%c";
-	ft_creat(x);
+	char	*x = "#include <unistd.h>%c#include <fcntl.h>%c#include <stdio.h>%c%c/*I'm so stupid*/%cint	ft_creat(char *x)%c{%c	int	fd = open(%ctmp_Colleen%c, O_WRONLY | O_CREAT | O_TRUNC, 0644);%c	if (fd == -1)%c		return(1);%c	dprintf(fd, x, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, x, 34, 10, 10, 10, 10, 10);%c	close(fd);%c	return(0);%c}%c%cint	main(void)%c{%c	/*but really!*/%c	char	*x = %c%s%c;%c	if (ft_creat(x))%c		return(1);%c	return (0);%c}%c";
+	if (ft_creat(x))
+		return(1);
 	return (0);
 }
