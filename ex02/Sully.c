3a1dda28438831a622b4b90f353a5758644cbfef
Eguelin <eguelin@student.42lyon.fr>
2025-01-24 23:05:49 +0100

    ex02: Simplify Sully.c by removing unused variable and correcting execl parameters

--- ex02/Sully.c
+++ ex02/Sully.c
@@ -5,9 +5,8 @@
 
 #define NAM "Sully_%d"
 #define FILE "%s.c"
-#define EXE "./%s"
 #define CMD "gcc -Wall -Wextra -Werror %s.c -o %s"
-#define CODE "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <fcntl.h>%c%c#define NAM %cSully_%cd%c%c#define FILE %c%cs.c%c%c#define EXE %c./%cs%c%c#define CMD %cgcc -Wall -Wextra -Werror %cs.c -o %cs%c%c#define CODE %c%s%c%c%cint main(void)%c{%c	int		i = %d;%c	int		fd;%c	int		pid;%c	char	nam[18];%c	char	file[20];%c	char	exe[20];%c	char	cmd[67];%c%c	if (i <= 0)%c		return (0);%c	snprintf(nam, 18, NAM, i - 1);%c	snprintf(file, 20, FILE, nam);%c	snprintf(exe, 20, EXE, nam);%c	snprintf(cmd, 67, CMD, nam, nam);%c	fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);%c	if (fd == -1)%c		return (1);%c	dprintf(fd, CODE, 10, 10, 10, 10, 10, 34, 37, 34, 10, 34, 37, 34, 10, 34, 37, 34, 10, 34, 37, 37, 34, 10, 34, CODE, 34, 10, 10, 10, 10, i - 1, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c	close(fd);%c	if (system(cmd))%c		return (1);%c	pid = fork();%c	if (pid == -1)%c		return (1);%c	if (pid == 0 && execl(exe, nam, NULL))%c		return (1);%c	return (0);%c}%c"
+#define CODE "#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <fcntl.h>%c%c#define NAM %cSully_%cd%c%c#define FILE %c%cs.c%c%c#define CMD %cgcc -Wall -Wextra -Werror %cs.c -o %cs%c%c#define CODE %c%s%c%c%cint main(void)%c{%c	int		i = %d;%c	int		fd;%c	int		pid;%c	char	nam[18];%c	char	file[20];%c	char	cmd[67];%c%c	if (i <= 0)%c		return (0);%c	snprintf(nam, 18, NAM, i - 1);%c	snprintf(file, 20, FILE, nam);%c	snprintf(cmd, 67, CMD, nam, nam);%c	fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);%c	if (fd == -1)%c		return (1);%c	dprintf(fd, CODE, 10, 10, 10, 10, 10, 34, 37, 34, 10, 34, 37, 34, 10, 34, 37, 37, 34, 10, 34, CODE, 34, 10, 10, 10, 10, i - 1, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c	close(fd);%c	if (system(cmd))%c		return (1);%c	pid = fork();%c	if (pid == -1)%c		return (1);%c	if (pid == 0 && execl(nam, nam, NULL))%c		return (1);%c	return (0);%c}%c"
 
 int main(void)
 {
@@ -16,26 +15,24 @@ int main(void)
 	int		pid;
 	char	nam[18];
 	char	file[20];
-	char	exe[20];
 	char	cmd[67];
 
 	if (i <= 0)
 		return (0);
 	snprintf(nam, 18, NAM, i - 1);
 	snprintf(file, 20, FILE, nam);
-	snprintf(exe, 20, EXE, nam);
 	snprintf(cmd, 67, CMD, nam, nam);
 	fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 		return (1);
-	dprintf(fd, CODE, 10, 10, 10, 10, 10, 34, 37, 34, 10, 34, 37, 34, 10, 34, 37, 34, 10, 34, 37, 37, 34, 10, 34, CODE, 34, 10, 10, 10, 10, i - 1, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+	dprintf(fd, CODE, 10, 10, 10, 10, 10, 34, 37, 34, 10, 34, 37, 34, 10, 34, 37, 37, 34, 10, 34, CODE, 34, 10, 10, 10, 10, i - 1, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 	close(fd);
 	if (system(cmd))
 		return (1);
 	pid = fork();
 	if (pid == -1)
 		return (1);
-	if (pid == 0 && execl(exe, nam, NULL))
+	if (pid == 0 && execl(nam, nam, NULL))
 		return (1);
 	return (0);
 }
