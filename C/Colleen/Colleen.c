dd5ec20ffda7856ce9ec3cd22a5be3ca23e706f2
Eguelin <eguelin@student.42lyon.fr>
2025-04-09 20:25:16 +0200

    fix: Replace ft_creat with ft_print to simplify output handling in Colleen.c

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -5,15 +5,9 @@
 /*
 I'm so stupid
 */
-int	ft_creat(char *x)
+int	ft_print(char *x)
 {
-	int	fd = open("tmp_Colleen", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-	if (fd == -1)
-		return(1);
-	dprintf(fd, x, 10, 34, x);
-	close(fd);
-	return(0);
+	return(printf(x, 10, 34, x));
 }
 
 int	main(void)
@@ -21,9 +15,9 @@ int	main(void)
 	/*
 	but really!
 	*/
-	char	*x = "#include <unistd.h>%1$c#include <fcntl.h>%1$c#include <stdio.h>%1$c%1$c/*%1$cI'm so stupid%1$c*/%1$cint	ft_creat(char *x)%1$c{%1$c	int	fd = open(%2$ctmp_Colleen%2$c, O_WRONLY | O_CREAT | O_TRUNC, 0644);%1$c%1$c	if (fd == -1)%1$c		return(1);%1$c	dprintf(fd, x, 10, 34, x);%1$c	close(fd);%1$c	return(0);%1$c}%1$c%1$cint	main(void)%1$c{%1$c	/*%1$c	but really!%1$c	*/%1$c	char	*x = %2$c%3$s%2$c;%1$c%1$c	if (ft_creat(x))%1$c		return(1);%1$c	return (0);%1$c}%1$c";
+	char	*x = "#include <unistd.h>%1$c#include <fcntl.h>%1$c#include <stdio.h>%1$c%1$c/*%1$cI'm so stupid%1$c*/%1$cint	ft_print(char *x)%1$c{%1$c	return(printf(x, 10, 34, x));%1$c}%1$c%1$cint	main(void)%1$c{%1$c	/*%1$c	but really!%1$c	*/%1$c	char	*x = %2$c%3$s%2$c;%1$c%1$c	if (ft_print(x) == -1)%1$c		return(1);%1$c	return (0);%1$c}%1$c";
 
-	if (ft_creat(x))
+	if (ft_print(x) == -1)
 		return(1);
 	return (0);
 }
