807c77e995d5599d048f18ea0742df1a66c07c8a
Eguelin <eguelin@student.42lyon.fr>
2025-04-11 14:24:53 +0200

    fix: Refactor Colleen assembly code for improved readability and structure

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,7 +1,7 @@
 ;Colleen in x86_64 assembly
 
 section .data
-	code db ";Colleen in x86_64 assembly%1$c%1$csection .data%1$c	code db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c	global main%1$c	extern printf%1$c	extern fflush%1$c%1$c	main:%1$c		;main function%1$c		push rbp%1$c		mov rbp, rsp%1$c%1$c		call ft_print%1$c%1$c		pop rbp%1$c%1$c		test rax, -1%1$c		js _error%1$c%1$c	_end:%1$c		mov rax, 60%1$c		xor rdi, rdi%1$c		syscall%1$c%1$c	_error:%1$c		mov rax, 60%1$c		mov rdi, 1%1$c		syscall%1$c%1$cft_print:%1$c		push rbp%1$c		mov rbp, rsp%1$c%1$c		lea rdi, [rel code]%1$c		mov rsi, 0xa%1$c		mov rdx, 0x22%1$c		lea rcx, [rel code]%1$c		call printf wrt ..plt%1$c%1$c		test rax, -1%1$c		js _end_ft_print%1$c%1$c		xor rdi, rdi%1$c		call fflush wrt ..plt%1$c%1$c	_end_ft_print:%1$c		mov rsp, rbp%1$c		pop rbp%1$c		ret%1$c", 0
+	code db ";Colleen in x86_64 assembly%1$c%1$csection .data%1$c	code db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c	global main%1$c	extern printf%1$c	extern fflush%1$c%1$c	main:%1$c		;main function%1$c		push rbp%1$c		mov rbp, rsp%1$c%1$c		call ft_print%1$c%1$c		mov rsp, rbp%1$c		pop rbp%1$c%1$c		test rax, -1%1$c		js _error%1$c%1$c	_end:%1$c		mov rax, 60%1$c		xor rdi, rdi%1$c		syscall%1$c%1$c	_error:%1$c		mov rax, 60%1$c		mov rdi, 1%1$c		syscall%1$c%1$c	ft_print:%1$c		push rbp%1$c		mov rbp, rsp%1$c%1$c		lea rdi, [rel code]%1$c		mov rsi, 0xa%1$c		mov rdx, 0x22%1$c		lea rcx, [rel code]%1$c		call printf wrt ..plt%1$c%1$c		test rax, -1%1$c		js _end_ft_print%1$c%1$c		xor rdi, rdi%1$c		call fflush wrt ..plt%1$c%1$c	_end_ft_print:%1$c		mov rsp, rbp%1$c		pop rbp%1$c		ret%1$c", 0
 
 section .text
 	global main
@@ -15,6 +15,7 @@ section .text
 
 		call ft_print
 
+		mov rsp, rbp
 		pop rbp
 
 		test rax, -1
@@ -30,7 +31,7 @@ section .text
 		mov rdi, 1
 		syscall
 
-ft_print:
+	ft_print:
 		push rbp
 		mov rbp, rsp
 
