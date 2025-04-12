7264be22849a70a8d2217d79342e12316877ea55
Eguelin <eguelin@student.42lyon.fr>
2025-04-12 17:34:10 +0200

    refactor: Refactor Grace.c to use CODE and FILE_NAME macros for improved clarity and maintainability

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -2,10 +2,11 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-#define STRING "#include <unistd.h>%1$c#include <fcntl.h>%1$c#include <stdio.h>%1$c%1$c#define STRING %2$c%3$s%2$c%1$c#define MAIN(str) int main(void) {char *x = str;int fd = open(%2$cGrace_kid.c%2$c, O_WRONLY | O_CREAT | O_TRUNC, 0644);if (fd == -1){return(1);};dprintf(fd, x, 10, 34, str);close(fd);return (0);}%1$c%1$c/*%1$cUSE YOUR BRAIN%1$c*/%1$cMAIN(STRING)%1$c"
-#define MAIN(str) int main(void) {char *x = str;int fd = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, 0644);if (fd == -1){return(1);};dprintf(fd, x, 10, 34, str);close(fd);return (0);}
+#define CODE "#include <unistd.h>%1$c#include <fcntl.h>%1$c#include <stdio.h>%1$c%1$c#define CODE %2$c%3$s%2$c%1$c#define FILE_NAME %2$cGrace_kid.c%2$c%1$c#define MAIN(code, file_name) int main(void) {char *x = code;int fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);if (fd == -1){return(1);};dprintf(fd, x, 10, 34, code);close(fd);return (0);}%1$c%1$c/*%1$cUSE YOUR BRAIN%1$c*/%1$cMAIN(CODE, FILE_NAME)%1$c"
+#define FILE_NAME "Grace_kid.c"
+#define MAIN(code, file_name) int main(void) {char *x = code;int fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);if (fd == -1){return(1);};dprintf(fd, x, 10, 34, code);close(fd);return (0);}
 
 /*
 USE YOUR BRAIN
 */
-MAIN(STRING)
+MAIN(CODE, FILE_NAME)
